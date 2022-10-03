f405da73aecb090b250387ba995bc64b824e78ac
SAINI Narender <narender.saini@soprasteria.com>
2022-10-03 12:47:33 +0530

    See changes made

--- Mockito/src/main/java/com/javatrainingschool/LibrarySpringBootApplication.java
+++ Mockito/src/main/java/com/javatrainingschool/LibrarySpringBootApplication.java
@@ -8,5 +8,6 @@ public class LibrarySpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibrarySpringBootApplication.class, args);
+		System.out.println("Change to see in GitHub");
 	}
 }
